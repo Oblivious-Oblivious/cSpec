4b9fcffe7f42fdb491dcbfd9c2f2e3e5acd3e80f
Oblivious <me262thanos@gmail.com>
2021-03-01 12:01:25 +0200

    TODO -> Need fix

--- examples/simple/simple.spec.c
+++ examples/simple/simple.spec.c
@@ -170,6 +170,7 @@ spec_suite({
 
 spec({
     run_spec_suite("all");
-    export_test_results("report.xml", "all", "xml");
+    /* TODO -> FIX ON MACOS CLANG MAKES A FORK BOMB */
+    /* export_test_results("report.xml", "all", "xml"); */
 });
 
