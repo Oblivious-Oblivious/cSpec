008f21cc336d3d0d0954a82e3d43890dbb4254c0
oblivious <thanos.papap42@gmail.com>
2024-08-16 02:32:38 +0300

    Removed unused definitions.

--- examples/simple/simple.c
+++ examples/simple/simple.c
@@ -1,19 +1,11 @@
 #include "simple.h"
 
-data dt;
+#include <stdio.h> /* printf */
 
 void debug_msg(void) { printf("This is called before all tests\n"); }
 
 void teardown_msg(void) { printf("This is called after all tests\n"); }
 
-void initializer(void) {
-  dt.i = 1;
-  dt.d = 2.0;
-  dt.s = "str";
-}
+void initializer(void) {}
 
-void destructor(void) {
-  dt.i = -1;
-  dt.d = 0;
-  dt.s = (void *)0;
-}
+void destructor(void) {}
--- examples/simple/simple.h
+++ examples/simple/simple.h
@@ -1,15 +1,6 @@
 #ifndef __SIMPLE_H_
 #define __SIMPLE_H_
 
-#include <stdio.h>
-
-typedef struct data {
-  int i;
-  double d;
-  char *s;
-} data;
-
-void setup_arrays(void);
 void debug_msg(void);
 void teardown_msg(void);
 void initializer(void);
